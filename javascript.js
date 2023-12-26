633db849d36540567a91eab79fbbeb5260b664d2
bouncychu <v.y.h.tong@gmail.com>
2023-12-26 21:35:37 +0800

    Add 16 by 16 grid using js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 //Javascript file for etch a sketch project
+
+//create a webpage of 16x16 grid of square divs
+const gridContainer = document.querySelector('.grid-container');
+let square = [];
+let row = [];
+
+for (i = 0; i < 16; i++) {
+    row[i] = document.createElement('div');
+    gridContainer.appendChild(row[i]);
+    for (j = 0; j < 16; j++) {
+        square[j] = document.createElement('div');
+        square[j].setAttribute('class', 'grid-square');
+        square[j].textContent = j;
+        row[i].appendChild(square[j]);
+    }
+}
